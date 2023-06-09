f6b9f65772519395f9e7845f1b5e00aadfbd72fd
ma-mahmudi <ma.mahmudi18@outlook.com>
2023-06-09 23:09:20 +0330

    push

--- adminBot/addReceiver.go
+++ adminBot/addReceiver.go
@@ -1,20 +1,31 @@
 package adminBot
 
-import telegramBot "gopkg.in/telebot.v3"
+import (
+	telegramBot "gopkg.in/telebot.v3"
+)
 
 func AddReceiver(bot *telegramBot.Bot) {
+
+	//ReceiversList := [...]struct {
+	//	Id   string
+	//	Name string
+	//}{{}}
 
 	bot.Handle("/add_receiver", func(context telegramBot.Context) error {
 		msg := context.Send(
 			"Enter the userId(s) you want to add to the list.\n" +
-				"The format should be as following:\n" +
+				"The format must be as following:\n\n" +
 				"Add:\n" +
-				"Id1:name1, Id2:name2, ...\n\n" +
-				"It's better for name to not contain '-'.")
+				"Id:Name, Id:Name, ...")
 
 		bot.Handle(telegramBot.OnText, func(context telegramBot.Context) error {
 			res := context.Text()
 			if len(res) > 5 && (res[:3] == "add" || res[:3] == "Add" || res[:3] == "ADD") {
+				//for i := 5; i <= len(res); i++ {
+				//	if res[i] == ':' {
+				//		res
+				//	}
+				//}
 				msg := context.Send("Receivers' list updated...")
 				return msg
 			}
@@ -23,14 +34,4 @@ func AddReceiver(bot *telegramBot.Bot) {
 
 		return msg
 	})
-
-	//bot.Handle(telegramBot.OnText, func(context telegramBot.Context) error {
-	//	res := context.Text()
-	//	if len(res) > 0 {
-	//		msg := context.Send("Ok")
-	//		return msg
-	//	}
-	//	return nil
-	//})
-
 }
